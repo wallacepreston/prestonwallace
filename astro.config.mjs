cb1727c4a2752afa2fa3375b71b620141f9faab3
Preston Wallace <wallace.preston@gmail.com>
2026-07-27 22:39:43 -0700

    fix noindex issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  site: 'https://prestonwallace.com',
   base: '/',
   output: 'static',
   integrations: [
